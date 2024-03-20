308c16fea83146e3b94b86bce015181babc2aec4
spaceTony9 <bilokon.fw7@gmail.com>
2024-03-20 18:10:03 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,49 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Product from "./Product.jsx";
-import {favouriteBooks, BookList} from "./FavoruteBooks.jsx";
-import Alert from "./Arert.jsx";
-
+import { useState } from 'react';
+import reactLogo from '../assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Product from './Product.jsx';
+import { favouriteBooks, BookList } from './FavoruteBooks.jsx';
+import Alert from './Arert.jsx';
 
 export default function App() {
-    return (
-        <div>
-            <h1>Best selling</h1>
+  const handleClick = evt => {
+    console.log(evt);
+  };
+  return (
+    <div>
+      <h1>Best selling</h1>
 
-            <Product
-                name="Tacos With Lime"
-                imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-                price={10.99}
-            />
-            <Product
-                name="Fries and Burger"
-                imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-                price={14.29}
-            />
-            <h1>Books of the week</h1>
-            <BookList books={favouriteBooks}/>
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+      <h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
 
-            <Alert variant="info">
-                Would you like to browse our recommended products?
-            </Alert>
-            <Alert variant="error">
-                There was an error during your last transaction
-            </Alert>
-            <Alert variant="success">
-                Payment received, thank you for your purchase
-            </Alert>
-            <Alert variant="warning">
-                Please update your profile contact information
-            </Alert>
-        </div>
-    );
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+      <button onClick={handleClick}>First button</button>
+      <button onClick={evt => console.log(evt)}>Second button</button>
+    </div>
+  );
 }
-
 
 // function App() {
 //   const [count, setCount] = useState(0)
